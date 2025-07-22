d129b34875721a6c8f7b4de59e25ccbfd7dba266
Vaghela Madhuri <vaghelamadhuri235@gmail.com>
2025-07-22 14:55:39 +0530

    Question.java
    Signed-off-by: Vaghela Madhuri  <vaghelamadhuri235@gmail.com>

---
+++ Question.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//print the sum of first n nutural number
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	    int sum = 0;
+		for (int i=0; i<n; i++) {
+		    //sum = sum+i;
+		    System.out.println(sum+i);
+		}
+	}
+}
