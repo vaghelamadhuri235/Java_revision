8c072bad533b07071b45275a67c2c66f26becc07
Vaghela Madhuri <vaghelamadhuri235@gmail.com>
2025-07-22 18:45:50 +0530

    Input.java
    Signed-off-by: Vaghela Madhuri  <vaghelamadhuri235@gmail.com>

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//input
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		System.out.print(name);
+	}
+}
