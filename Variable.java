329af35477d99f49127ce05e58773a0635075f37
Vaghela Madhuri <vaghelamadhuri235@gmail.com>
2025-07-22 14:44:48 +0530

    Variable.java
    Signed-off-by: Vaghela Madhuri  <vaghelamadhuri235@gmail.com>

---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		// variable
+		String name = "madhuri";
+		int a = 10;
+		int b = 10;
+		double c = 20.9;
+		float d = 29;
+		
+		System.out.println(name);
+		System.out.println(a);
+		System.out.println(b);
+	    System.out.println(c);
+		System.out.println(d);
+	}
+}
