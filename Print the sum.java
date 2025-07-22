c909257e1b8d3472e1d08d2bff3e7dab3b926fc8
Vaghela Madhuri <vaghelamadhuri235@gmail.com>
2025-07-22 18:42:57 +0530

    Print the sum.java
    Signed-off-by: Vaghela Madhuri  <vaghelamadhuri235@gmail.com>

---
+++ Print the sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		//take a two variable a and b print the sum
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
+		
+   	int c = 40;
+	  int d = 80;
+	   System.out.println(c+d);
+	}
+}
