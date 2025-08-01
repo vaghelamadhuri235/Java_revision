a87445c482dfc93a0d78cd42e75aae4f90fb1a33
Vaghela Madhuri <vaghelamadhuri235@gmail.com>
2025-08-01 10:23:03 +0530

    Array.java
    Signed-off-by: Vaghela Madhuri  <vaghelamadhuri235@gmail.com>

---
+++ Array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Array {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//int marks = sc.nextInt();
+		int [ ] marks = new int [3];
+		//int marks [] = new int [3];
+		//int marks [] = {56, 67, 45};
+		//marks [0] = 89;
+		//marks [1] = 90;
+		//marks [2] = 78;
+
+		//System.out.println(marks); // garbage valvecalculatesum
+
+		//System.out.println(marks[0]);
+		//System.out.println(marks[1]);
+		//System.out.println(marks[2]);
+		for (int i=0; i<3; i++) {
+		    System.out.println(marks[i]);
+      }
+		 }
+   }
